793bd9827dd3f556ad5f54d9b857c5c8c76c3385
Sandeep <rawats@opentext.com>
2024-10-03 20:44:22 +0530

    plus one

---
+++ main/plus-one.go
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=66 lang=golang
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+func plusOne(digits []int) []int {
+	n := len(digits)
+	for i:=n-1 ; i>=0 ;i--{
+		if digits[i]<9{
+			digits[i]++
+			return digits
+		}
+		digits[i]=0
+	}
+	digits = append([]int{1}, digits...)
+	return digits    
+}
+// @lc code=end
+
